e602ea8a7ecd68619d58b945a6c1bfa2f2e9de24
DreamWalkerJK <lunatic_jk@qq.com>
2022-12-04 18:29:47 +0800

    添加项目文件。

---
+++ MemoryCache_Core/MemoryCacheDemo.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemoryCache_Core
+{
+    public class MemoryCacheDemo
+    {
+        private static IMemoryCache _cache = new MemoryCache(new MemoryCacheOptions());
+
+        #region 缓存过期时间类型
+        /// <summary>
+        /// 永不过期
+        /// 不清理该缓存，该缓存就一直有效
+        /// </summary>
+        public static void SetNeverExpire(string key, string value)
+        {
+            _cache.Set(key, value);
+        }
+
+        /// <summary>
+        /// 绝对过期
+        /// </summary>
+        public static void SetAbsoluteExpiration(string key, string value, DateTime absoluteTime)
+        {
+            _cache.Set(key, value, absoluteTime);
+        }
+
+        /// <summary>
+        /// 相对过期
+        /// </summary>
+        public static void SetExpirationTimeRelativeToThePresent(string key, string value, TimeSpan timeSpan)
+        {
+            _cache.Set(key, value, timeSpan);
+        }
+
+        /// <summary>
+        /// 滑动过期
+        /// 设定的时间内未被使用则失效，使用后过期时间则被重新刷新
+        /// </summary>
+        public static void SetSlidingExpiration(string key, string value, TimeSpan slidTimeSpan, DateTimeOffset absoluteDateTimeOffset)
+        {
+            _cache.Set(key, value, new MemoryCacheEntryOptions()
+            {
+                SlidingExpiration = slidTimeSpan,
+                // 设置缓存项的绝对到期日期，为当前缓存设置后的10分钟
+                AbsoluteExpiration = absoluteDateTimeOffset
+            });
+        }
+        #endregion
+
+        #region 获取缓存值
+        public static string GetCacheByKey(string key)
+        {
+           return _cache.Get(key).ToString();
+        }
+
+        public static void GetCacheByKey(string key, out string value)
+        {
+            if(!_cache.TryGetValue(key, out value))
+            {
+                throw new Exception("该缓存不存在或者已过期");
+            }
+        }
+        #endregion
+
+        #region 清除缓存值
+        public static void RemoveCacheByKey(string key)
+        {
+            if(!_cache.TryGetValue(key, out _))
+            {
+                _cache.Remove(key);
+            }
+        }
+        #endregion
+    }
+}
---
+++ MemoryCache_Core/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MemoryCache_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("设置永不过期");
+            MemoryCacheDemo.SetNeverExpire("NeverExpire", "1");
+            Console.WriteLine("设置绝对过期");
+            MemoryCacheDemo.SetAbsoluteExpiration("AbsoluteExpiration", "2", new DateTime(2022, 12, 04, 18, 30, 00));
+            Console.WriteLine("设置相对过期");
+            MemoryCacheDemo.SetExpirationTimeRelativeToThePresent("ExpirationTimeRelativeToThePresent", "3", new TimeSpan(0, 0, 1));
+            Console.WriteLine("设置滑动过期");
+            MemoryCacheDemo.SetSlidingExpiration("SlidingExpiration", "4", new TimeSpan(0, 0, 1), DateTimeOffset.Now.AddMinutes(1));
+
+            Console.WriteLine("获取{0}缓存值：{1}", "NeverExpire", MemoryCacheDemo.GetCacheByKey("NeverExpire"));
+            Console.WriteLine("获取{0}缓存值：{1}", "AbsoluteExpiration", MemoryCacheDemo.GetCacheByKey("AbsoluteExpiration"));
+            Console.WriteLine("获取{0}缓存值：{1}", "ExpirationTimeRelativeToThePresent", MemoryCacheDemo.GetCacheByKey("ExpirationTimeRelativeToThePresent"));
+            Console.WriteLine("获取{0}缓存值：{1}", "SlidingExpiration", MemoryCacheDemo.GetCacheByKey("SlidingExpiration"));
+
+            Console.WriteLine("删除缓存");
+            MemoryCacheDemo.RemoveCacheByKey("NeverExpire");
+            MemoryCacheDemo.RemoveCacheByKey("AbsoluteExpiration");
+            MemoryCacheDemo.RemoveCacheByKey("ExpirationTimeRelativeToThePresent");
+            MemoryCacheDemo.RemoveCacheByKey("SlidingExpiration");
+
+            Console.ReadKey();
+        }
+    }
+}
